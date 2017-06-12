5a7d2356ffce0b9485a54c84cd44ec53b11e902e
V Pena <victor@vpena.com>
2017-06-11 21:31:17 -0400

    Your Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { DataService } from '../services/data.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements  OnInit{
+  title = 'Test of API Access';
+
+  constructor(private dataService: DataService) { }
+
+  // the data returned from the API
+  public results:[string];
+
+  ngOnInit(){
+
+    return this.getDocuments();
+
+  }
+
+  getDocuments(){
+    console.log("...getting test data from data service: ");
+
+    return this.dataService.getDocuments()
+      .then(response => this.results = response);
+  }
 }
---
+++ src/services/data.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+
+
+@Injectable()
+export class DataService {
+
+  // PRODUCTION
+  // private dataURL = 'https://api.occwatch.com/api/';
+
+  // DEVELOPMENT
+  private dataURL = 'http://localhost:5000/api/';
+  private fullPath: string;
+
+  constructor(private http: Http) { }
+
+  // test method
+  getDocuments(){
+    this.fullPath = this.dataURL + "test";
+
+    console.log("Inside data.service.getDocuments()");
+    console.log("... full path of request: ", this.fullPath);
+
+    return this.http.get(this.fullPath)
+      .toPromise()
+      .then(result => result.json())
+      // .catch(this.handleError);
+      .catch((res: Response) => this.handleError(res));
+  }
+
+  handleError(error: any){
+
+    // console.log('An error occurred!');
+    // console.log(error);
+    return Promise.reject(error.message || error);
+
+  }
+
+
+
+}
